b3eba0eb1b2c114355c24cbf4abbbb58e4d34a53
hugoorlando <hugo.orlando2030@gmail.com>
2022-09-07 12:53:18 -0300

    intro javascript

---
+++ 02-counter-app/src/App.jsx
@@ -0,0 +1,8 @@
+// Todo en React empieza con un Functional Component
+// El standard es trabajar con Functional Component
+function App() {
+  // document.createElement....
+  return <h1>Hello Vite</h1>;
+}
+
+export default App;
