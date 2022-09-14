81d8af7733761fe2de798af02c59ec4a595c4423
hugoorlando <hugo.orlando2030@gmail.com>
2022-09-14 09:32:20 -0300

    Import, export y funciones comunes de arreglos

--- 02-intro-javascript/src/bases/08-imp-exp.js
+++ 02-intro-javascript/src/bases/08-imp-exp.js
@@ -1,3 +1,17 @@
 import { hollywood } from '../api/hollywood';
 
 // https://es.javascript.info/array-methods#filter
+
+const getActorsById = id => {
+  return hollywood.find(actor => actor.id === id);
+};
+let res = getActorsById(3);
+console.log(res);
+
+res = getActorsById(4);
+console.log(res);
+
+const getActorsByLetter = () => {
+  return hollywood.filter(actor => actor.name.includes('L'));
+};
+console.log(getActorsByLetter());
