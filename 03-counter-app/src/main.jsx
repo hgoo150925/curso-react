99369938ab95c309cb6a0a62033aa7adee20f692
hugoorlando <hugo.orlando2030@gmail.com>
2022-09-14 11:57:50 -0300

    hola en react

--- 03-counter-app/src/main.jsx
+++ 03-counter-app/src/main.jsx
@@ -1,10 +1,16 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.css'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+
+// Functional components vs Class components
+// https://medium.com/@brenditech/functional-components-vs-class-components-e6eaecba7562
+
+function App() {
+  // document.createElement...
+  return <h1>Hola</h1>;
+}
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
-)
+);
