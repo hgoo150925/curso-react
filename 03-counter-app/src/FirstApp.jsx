087b30b7a1bfa8cbd12493e50d6864d571fc63f6
hugoorlando <hugo.orlando2030@gmail.com>
2022-09-14 12:43:12 -0300

    first app

--- 03-counter-app/src/FirstApp.jsx
+++ 03-counter-app/src/FirstApp.jsx
@@ -1,8 +1,20 @@
 export const FirstApp = () => {
+  // Esta variable es parte del componente
+  // React no reprocesa (renderizar nuevamente) todo lo que se encuentre fuera de este componente
+  // React renderiza todo lo que se encuentre dentro de este componente
+  const newMessage = "What's up!";
+
+  const newObj = {
+    name: 'Ryan Gosling',
+    country: 'United States',
+  };
+
   return (
     <>
-      <h2>Ryan Gosling</h2>
-      <h3>Canada</h3>
+      {/* dentro de las llaves pueden ir expresiones de JS que no sean objetos */}
+      <h2>{newMessage}</h2>
+      <h3>{newObj.name}</h3>
+      <code>{JSON.stringify(newObj)}</code>
     </>
   );
 };
