f05d9c93dc6696826ca0960b72120d6922afe6a4
hugoorlando <hugo.orlando2030@gmail.com>
2022-09-15 14:35:02 -0300

    test con async-await

--- 03-counter-app/src/base-pruebas/11-async-await.js
+++ 03-counter-app/src/base-pruebas/11-async-await.js
@@ -1,9 +1,6 @@
-// const getImagenPromesa = () => new Promise( resolve => resolve('https://ajskdhaskjdhajs.com') )
-// getImagenPromesa().then( console.log );
-
-const getImagen = async () => {
+export const getImage = async () => {
   try {
-    const apiKey = 'C1khQe3Z7R1W2lfTO9myKeuShdqFYSGC';
+    const apiKey = '';
     const resp = await fetch(
       `http://api.giphy.com/v1/gifs/random?api_key=${apiKey}`
     );
@@ -11,13 +8,12 @@ const getImagen = async () => {
 
     const { url } = data.images.original;
 
-    const img = document.createElement('img');
-    img.src = url;
-    document.body.append(img);
+    return url;
   } catch (error) {
     // manejo del error
-    console.error(error);
+    // console.error(error)
+    return 'No se encontro la imagen';
   }
 };
 
-getImagen();
+getImage();
